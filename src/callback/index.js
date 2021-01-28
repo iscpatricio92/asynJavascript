e7fbb81083921bd3f86b4563f527df4816ff90ec
Patricio <Patricio@C02T14VNG8WN.local>
2021-01-28 00:29:55 -0600

    ejemplo 1 callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,26 @@
+function sum(num1, num2){
+    return num1 + num2
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+console.log("SUMA: "+ calc(6, 2, sum));
+
+
+function date(callback) {
+    console.log("DATE CALLBACK");
+    console.log(new Date);
+    setTimeout(function(){
+        let date= new Date
+        callback(date)
+    },3000)
+}
+
+function printDate(dateNow) {
+    console.log("PRINTDATE");
+    console.log(dateNow);
+}
+
+date(printDate)
